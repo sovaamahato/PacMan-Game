92dce0a4990c944b9ce40bffab736d20a61e41c0
sova <mahatoshobha143@gmail.com>
2023-05-04 12:11:24 +0545

    some function defination added

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -2,13 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MyPanel extends JPanel implements ActionListener {
 
 
+    private Color mazeColor;
     private Dimension d;
     private final Font smallFont = new Font("Helvetica", Font.BOLD, 14);
     private boolean inGame = false;
+    private boolean dying = false;
     private final int BLOCK_SIZE = 24;
 
     //15 ota rows and column
@@ -17,21 +21,24 @@ public class MyPanel extends JPanel implements ActionListener {
 
     private final int MAX_GHOSTS = 12;
 
-    private final int PACMAN_SPEED=6;
+    private final int PACMAN_SPEED = 6;
 
-    private final int N_GHOST=6;
-    private int  lives,score;
+    private  int N_GHOSTS = 6;
+    private int lives, score;
 
     private int[] dx, dy;
 
-    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy;
+    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
     private Image heart, ghost;
-    private Image up,down,left,right;
-    private int pacman_x,pacman_y ,pacmand_x,pacmand_y;
-    private int red_dx,red_dy;
-    private  final int validSpeed[]={1,2,3,4,6,8};
-    private int currentSpeed =3;
-    private int maxSpeed =6;
+    //    private Image up,down,left,right;
+    private Image pacman1, pacman2up, pacman2left, pacman2right, pacman2down;
+    private Image pacman3up, pacman3down, pacman3left, pacman3right;
+    private Image pacman4up, pacman4down, pacman4left, pacman4right;
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+    private int req_dx, req_dy;
+    private final int validSpeed[] = {1, 2, 3, 4, 6, 8};
+    private int currentSpeed = 3;
+    private int maxSpeed = 6;
     private short[] screenData;
     private Timer timer;
 
@@ -60,14 +67,109 @@ public class MyPanel extends JPanel implements ActionListener {
             9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 25, 24, 24, 24, 28
     };
 
-
-
-
-
-
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public MyPanel() {
+        loadImages();
+        initVariables();
+        addKeyListener(new TAdapter());
+        setFocusable(true);
 
     }
-}
+
+    private void loadImages() {
+        ghost = new ImageIcon("src/resources/images/ghost.png").getImage();
+        pacman1 = new ImageIcon("src/resources/images/pacman.png").getImage();
+        pacman2up = new ImageIcon("src/resources/images/up1.png").getImage();
+        pacman3up = new ImageIcon("src/resources/images/up2.png").getImage();
+        pacman4up = new ImageIcon("src/resources/images/up3.png").getImage();
+        pacman2down = new ImageIcon("src/resources/images/down1.png").getImage();
+        pacman3down = new ImageIcon("src/resources/images/down2.png").getImage();
+        pacman4down = new ImageIcon("src/resources/images/down3.png").getImage();
+        pacman2left = new ImageIcon("src/resources/images/left1.png").getImage();
+        pacman3left = new ImageIcon("src/resources/images/left2.png").getImage();
+        pacman4left = new ImageIcon("src/resources/images/left3.png").getImage();
+        pacman2right = new ImageIcon("src/resources/images/right1.png").getImage();
+        pacman3right = new ImageIcon("src/resources/images/right2.png").getImage();
+        pacman4right = new ImageIcon("src/resources/images/right3.png").getImage();
+    }
+
+    private void initVariables() {
+
+        screenData = new short[N_BLOCKS * N_BLOCKS];
+        mazeColor = new Color(5, 100, 5);
+        d = new Dimension(400, 400);
+        ghost_x = new int[MAX_GHOSTS];
+        ghost_dx = new int[MAX_GHOSTS];
+        ghost_y = new int[MAX_GHOSTS];
+        ghost_dy = new int[MAX_GHOSTS];
+        ghostSpeed = new int[MAX_GHOSTS];
+        dx = new int[4];
+        dy = new int[4];
+
+        timer = new Timer(40, this);
+        timer.start();
+    }
+
+    private void initGame() {
+
+        lives = 3;
+        score = 0;
+        initLevel();
+        N_GHOSTS = 6;
+        currentSpeed = 3;
+    }
+    private void initLevel() {
+
+        int i;
+        for (i = 0; i < N_BLOCKS * N_BLOCKS; i++) {
+            screenData[i] = levelData[i];
+        }
+//ths function is left to define
+        //continueLevel();
+    }
+
+
+
+    class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT) {
+                    req_dx = -1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    req_dx = 1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_UP) {
+                    req_dx = 0;
+                    req_dy = -1;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    req_dx = 0;
+                    req_dy = 1;
+                } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
+                    inGame = false;
+                } else if (key == KeyEvent.VK_PAUSE) {
+                    if (timer.isRunning()) {
+                        timer.stop();
+                    } else {
+                        timer.start();
+                    }
+                }
+            } else {
+                if (key ==KeyEvent.VK_SPACE) {
+                    inGame = true;
+                    initGame();
+                }
+            }
+        }
+    }
+
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
